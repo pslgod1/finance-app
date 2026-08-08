15ce3e51bcab9444583b2914776ce1aab7c186f1
pslgod1 <ivankulanov4@gmail.com>
2026-08-08 18:54:41 +0300

    Добавли модель статистики

--- internal/model/models.go
+++ internal/model/models.go
@@ -19,3 +19,9 @@ type Transaction struct {
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 }
+
+type Statistics struct {
+	TotalIncome  float64 `json:"total_income"`
+	TotalExpense float64 `json:"total_expense"`
+	Balance      float64 `json:"balance"`
+}
